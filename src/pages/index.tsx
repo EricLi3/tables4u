bd1184afe835b0e7be4725cb4882d93e9ab0df98
EricLi3 <ericlibeibei@gmail.com>
2024-11-19 18:49:22 -0500

    With activeRests from database and option to request more.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@
 
 import React from "react";
 import Link from "next/link";
+import { Button } from "@mui/material";
 import "@/app/globals.css";
 import RestaurantTable from "@/app/components/RestaurantTable";
 
 export default function Home() {
   return (
     <main className="flex w-screen h-screen flex-col items-center justify-between p-24">
-      
+
       <div className="top-left-button">
         <Link href="/">
           <img src="../logo.png" alt="Home Button" className="logo" />
@@ -34,6 +35,20 @@ export default function Home() {
             </li>
           </ul>
         </nav>
+      </div>
+
+      <div className="findAndCancel">
+      <Button
+        variant="contained"
+        color="secondary">
+        Find Reservation
+      </Button>
+      <br></br>
+      <Button
+        variant="contained"
+        color="error">
+        Cancel Reservation
+      </Button>
       </div>
     </main>
   );

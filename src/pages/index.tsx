3c45c6048bf31fd42e0a5a79bc6b9e1cf4c3bb93
EricLi3 <ericlibeibei@gmail.com>
2024-11-18 23:48:42 -0500

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import '@/app/globals.css';
+
+
+function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="top-left-container">
+        <Link href="/">
+          <button className="top-left-button">
+            <img src="logo.png" alt="Home Button" className="logo" />
+          </button>
+        </Link>
+        <div className="loginNav">
+          <nav>
+            <ul>
+              <li><Link href="/login/admin">Admin Login</Link></li>
+              <li><Link href="/login/manager">Manager Login</Link></li>
+              <li><Link href="/login/consumer">Consumer Login</Link></li>
+            </ul>
+          </nav>
+        </div>
+        
+        <h1>Tables4u</h1>
+
+      </div>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/pages/login/consumer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+import '@/app/globals.css';
+
+function ConsumerLogin() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/">
+        <button className="top-left-button">
+          <img src="../logo.png" alt="Home Button" className="logo" />
+        </button>
+      </Link>
+
+      <h1>Tables4u - consumer Login Page</h1>
+      <nav>
+        <ul>
+          {/* <li><Link href="/login/admin">Admin Login</Link></li>
+          <li><Link href="/login/manager">Manager Login</Link></li>
+          <li><Link href="/login/consumer">Consumer Login</Link></li> */}
+        </ul>
+      </nav>
+    </main>
+  );
+}
+
+export default ConsumerLogin;

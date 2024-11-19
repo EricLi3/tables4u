bf61e1e3893a00c94ea3bb5b2393c1758593b948
bambusgs <jjandus@wpi.edu>
2024-11-19 13:39:18 -0500

    some CSS edits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,38 @@
 
 import React from "react";
 import Link from "next/link";
-import '@/app/globals.css';
+import "@/app/globals.css";
 import RestaurantTable from "@/app/components/RestaurantTable";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="top-left-container">
+    <main className="flex w-screen h-screen flex-col items-center justify-between p-24">
+      
+      <div className="top-left-button">
         <Link href="/">
-          <button className="top-left-button">
-            <img src="logo.png" alt="Home Button" className="logo" />
-          </button>
+          <img src="logo.png" alt="Home Button" className="logo" />
         </Link>
-        <div className="loginNav">
-          <nav>
-            <ul>
-              <li><Link href="/login/admin">Admin Login</Link></li>
-              <li><Link href="/login/manager">Manager Login</Link></li>
-              <li><Link href="/login/consumer">Consumer Login</Link></li>
-            </ul>
-          </nav>
-        </div>
+      </div>
 
+      <div className="table">
         <h1>Tables4u</h1>
         <RestaurantTable />
+      </div>
+
+      <div className="loginNav">
+        <nav>
+          <ul>
+            <li>
+              <Link href="/login/admin">Admin Login</Link>
+            </li>
+            <li>
+              <Link href="/login/manager">Manager Login</Link>
+            </li>
+            <li>
+              <Link href="/login/consumer">Consumer Login</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </main>
   );

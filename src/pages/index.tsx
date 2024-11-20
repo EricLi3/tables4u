9a7d268919a626088d2a859896d30fa9e1ccb2e8
bambusgs <jjandus@wpi.edu>
2024-11-19 19:25:53 -0500

    finished login menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Link from "next/link";
 import "@/app/globals.css";
 import RestaurantTable from "@/app/components/RestaurantTable";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Home() {
   return (
@@ -19,22 +20,33 @@ export default function Home() {
         <RestaurantTable />
       </div>
 
-      <div className="loginNav">
-        <nav>
+      <div className="loginNav centering-div">
+        <div className="centering-div div-horiz">
+          <Link href="/login/admin">
+            <img
+              src="admin.png"
+              alt="Admin Login Button"
+              className="loginImg"
+            />
+            <div className="icon-center">
+              <MenuIcon />
+            </div>
+          </Link>
 
-              <Link href="/login/admin">Admin Login
-              <img src="admin.png" alt="Admin Login Button" className="loginImg" /></Link>
-
-              <Link href="/login/manager">Manager Login
-              <img src="manager.png" alt="Manager Login Button" className="loginImg" />
-              </Link>
-
-              <Link href="/login/consumer">Find Reservation</Link>
-
-        </nav>
-
+          <Link href="/login/manager">
+            <img
+              src="manager.png"
+              alt="Manager Login Button"
+              className="loginImg"
+            />
+            <div className="icon-center">
+              <MenuIcon />
+            </div>
+          </Link>
+        </div>
+        <Link href="/login/consumer">Find Reservation</Link>
         <Link href="/createRestaurant">
-          <button className="btn_dark inline-block">Create Restaurant</button>
+          <button className="btn_dark">Create Restaurant</button>
         </Link>
       </div>
     </main>

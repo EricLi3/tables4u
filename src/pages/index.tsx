b4f76f3bcd604ea7ecd0620d8f59a024819d2db2
bambusgs <jjandus@wpi.edu>
2024-11-19 19:53:13 -0500

    designed login pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,17 @@ export default function Home() {
 
       <div className="loginNav centering-div">
         <div className="centering-div div-horiz">
+          
+          <Link href="/login/manager">
+            <img
+              src="manager.png"
+              alt="Manager Login Button"
+              className="loginImg"
+            />
+            <div className="icon-center">
+              <MenuIcon />
+            </div>
+          </Link>
           <Link href="/login/admin">
             <img
               src="admin.png"
@@ -33,17 +44,8 @@ export default function Home() {
             </div>
           </Link>
 
-          <Link href="/login/manager">
-            <img
-              src="manager.png"
-              alt="Manager Login Button"
-              className="loginImg"
-            />
-            <div className="icon-center">
-              <MenuIcon />
-            </div>
-          </Link>
         </div>
+
         <Link href="/login/consumer">Find Reservation</Link>
         <Link href="/createRestaurant">
           <button className="btn_dark">Create Restaurant</button>
--- src/pages/login/admin.tsx
+++ src/pages/login/admin.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import Link from "next/link";
 
-import '@/app/globals.css';
+import "@/app/globals.css";
+import TextField from "@mui/material/TextField";
 
 function AdminLogin() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex  w-screen h-screen flex-col items-center justify-between p-24">
       <div className="top-left-button">
         <Link href="/">
           <img src="../logo.png" alt="Home Button" className="logo" />
         </Link>
       </div>
+      
+      <div className="centering-div login-fields">
+        <img src="../admin.png" alt="Admin Icon" className="loginImg" />
 
-      <h1>Tables4u - Admin Login Page</h1>
-      <nav>
-        <ul>
-          {/* <li><Link href="/login/admin">Admin Login</Link></li>
-          <li><Link href="/login/manager">Manager Login</Link></li>
-          <li><Link href="/login/consumer">Consumer Login</Link></li> */}
-        </ul>
-      </nav>
+        <TextField
+          required
+          id="outlined-required"
+          color="secondary"
+          label="Admin Username"
+        />
+
+        <TextField
+          required
+          id="outlined-required"
+          color="secondary"
+          label="Password"
+        />
+
+        <Link href="/admin">
+          <button className="btn_secondary">Login</button>
+        </Link>
+      </div>
     </main>
   );
 }

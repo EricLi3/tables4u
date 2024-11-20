91d1a1ec3b2828312edfcc22b40892ac4c433488
Eric Li <53413233+EricLi3@users.noreply.github.com>
2024-11-19 20:41:50 -0500

    Update manager.tsx

--- src/pages/login/manager.tsx
+++ src/pages/login/manager.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Box, TextField, Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 import '@/app/globals.css';
 import TextField from "@mui/material/TextField";

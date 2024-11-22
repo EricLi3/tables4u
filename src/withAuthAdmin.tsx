8ac5fb4550faac8c3a962d1f2e57b9cc2d082787
EricLi3 <ericlibeibei@gmail.com>
2024-11-21 19:37:43 -0500

    Passed linter with needing display name

--- src/withAuthAdmin.tsx
+++ src/withAuthAdmin.tsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-const withAuthAdmin = (WrappedComponent: any) => {
-  return (props: any) => {
+const withAuthAdmin = (WrappedComponent: React.ComponentType) => {
+  const WithAuthAdmin = (props: JSX.IntrinsicAttributes) => {
     const router = useRouter();
 
     useEffect(() => {
       const token = sessionStorage.getItem("jwtToken");
       if (!token) {
-        router.push("/login/admin"); // or somewhere else
+        router.push("/login/admin");
       }
-    }, []);
+    }, [router]);
 
     return <WrappedComponent {...props} />;
   };
-}
+
+  WithAuthAdmin.displayName = `WithAuthAdmin(${WrappedComponent.displayName || WrappedComponent.name || "Component"})`;
+
+  return WithAuthAdmin;
+};
 
 export default withAuthAdmin;

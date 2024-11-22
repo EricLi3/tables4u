89ba04f8ed7b4b3c5c3e770cf507b60308c4ebbf
EricLi3 <ericlibeibei@gmail.com>
2024-11-22 14:43:32 -0500

    Added lambda function to get info about one rest given uuid

---
+++ getInfoOneRest/index.mjs
@@ -0,0 +1,49 @@
+import mysql from 'mysql';
+
+export const handler = async (event) => {
+    // Specify credentials
+    const pool = mysql.createPool({
+        host: "calculatordb1.c7woyy8ecbg9.us-east-2.rds.amazonaws.com",
+        user: "admin",
+        password: "Netro7887",
+        database: "tables4u"
+    });
+
+    const rest_uuid = event.rest_uuid;
+
+
+    const GetRest = (rest_uuid) => {
+        return new Promise((resolve, reject) => {
+            pool.query("SELECT * FROM Restaurants WHERE restUUID=?", [rest_uuid], (error, rows) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(rows); // Return rows if successful
+            });
+        });
+    };
+
+    let response;
+
+    try {
+        // Wait for the query result
+        const rows = await GetRest(rest_uuid);
+        console.log('Query result:', rows);
+
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(rows)
+        };
+    } catch (error) {
+        console.error("ERROR:", error);
+        response = {
+            statusCode: 400,
+            body: JSON.stringify({ error: error.message })
+        };
+    } finally {
+        // Close the pool after all operations are done
+        pool.end();
+    }
+
+    return response;
+};

5aa0a109a13fe2931423268e78348cc1f9572e3c
bambusgs <jjandus@wpi.edu>
2024-12-02 13:52:52 -0500

    login page redirects with email

--- src/pages/login/consumer.tsx
+++ src/pages/login/consumer.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import "@/app/globals.css";
 import TextField from "@mui/material/TextField";
 
 function ConsumerLogin() {
+  const router = useRouter();
+  const [email, setEmail] = React.useState("");
+  const [confCode, setConfCode] = React.useState("");
+
+  const handleClick = () => {
+    if (email === "" || confCode === "") {
+      alert("Please fill in all fields");
+      return;
+    }
+    router.push("/findReservation?email=" + email + "&confCode=" + confCode);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="top-left-button">
@@ -19,6 +32,8 @@ function ConsumerLogin() {
           id="outlined-required"
           color="secondary"
           label="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <TextField
@@ -26,11 +41,13 @@ function ConsumerLogin() {
           id="outlined-required"
           color="secondary"
           label="Confirmation Code"
+          value={confCode}
+          onChange={(e) => setConfCode(e.target.value)}
         />
 
-        <Link href="/findReservation">
-          <button className="btn_secondary">Find Reservation</button>
-        </Link>
+        <button className="btn_secondary" onClick={() => handleClick()}>
+          Find Reservation
+        </button>
       </div>
     </main>
   );

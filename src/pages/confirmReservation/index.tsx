2b37b1f6965d58f9a633801014e357df5b44b16f
bambusgs <jjandus@wpi.edu>
2024-12-08 15:32:12 -0500

    made a nice dialog

--- src/pages/confirmReservation/index.tsx
+++ src/pages/confirmReservation/index.tsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 
 import "@/app/globals.css";
 import TextField from "@mui/material/TextField";
+import CustomDialog from "@/app/components/CustomDialog";
 
 import axios from "axios";
 
@@ -12,50 +13,30 @@ const instance = axios.create({
   baseURL: "https://jz4oihez68.execute-api.us-east-2.amazonaws.com/initial",
 });
 
-// Function to create a reservation (reservationUUID, restUUID, benchUUID, reservationDateTime, startTime, email, confirmationCode, groupSize)
-const createReservation = async (
-  resev_uuid: string,
+// TODO - Modify to take in Start and End time as parameters.
+const findOpenTable = async (
   rest_uuid: string,
-  bench_uuid: string,
-  reservation_date_time: string,
+  group_size: string,
   start_time: number,
-  email: string,
-  confirmation_code: string,
-  group_size: number
+  end_time: number
 ) => {
   try {
-    const response = await instance.post("/makeReservation", {
-      resev_uuid,
+    const response = await instance.post("/findTable", {
       rest_uuid,
-      bench_uuid,
-      reservation_date_time,
-      start_time,
-      email,
-      confirmation_code,
       group_size,
+      start_time,
+      end_time,
     });
-    console.log("Reservation created:", response.data);
-    alert("Reservation created successfully! \nConfirmation code: " + confirmation_code);
-    router.push("/");
-
-  } catch (error) {
-    console.error("Failed to create reservation:", error);
-    alert("Failed to create reservation. Please try again later.");
-  }
-};
-// TODO - Modify to take in Start and End time as parameters. 
-const findOpenTable = async (rest_uuid: string, group_size: string,  start_time: number, end_time: number) => {
-  try {
-    const response = await instance.post("/findTable", { rest_uuid, group_size, start_time, end_time });
     console.log("Open table found:", response.data);
     return response.data;
   } catch (error) {
     console.error("Failed to find open table:", error);
   }
-}
+};
 const generateConfirmationCode = () => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
@@ -73,8 +54,42 @@ const getRestaurantInfo = async (rest_uuid: string) => {
 };
 
 function ConfirmReservation() {
+  // Function to create a reservation (reservationUUID, restUUID, benchUUID, reservationDateTime, startTime, email, confirmationCode, groupSize)
+  const createReservation = async (
+    resev_uuid: string,
+    rest_uuid: string,
+    bench_uuid: string,
+    reservation_date_time: string,
+    start_time: number,
+    email: string,
+    confirmation_code: string,
+    group_size: number
+  ) => {
+    try {
+      const response = await instance.post("/makeReservation", {
+        resev_uuid,
+        rest_uuid,
+        bench_uuid,
+        reservation_date_time,
+        start_time,
+        email,
+        confirmation_code,
+        group_size,
+      });
+      console.log("Reservation created:", response.data);
+      setConfirmationCode(confirmation_code);
+      setOpenDialog(true);
+    } catch (error) {
+      console.error("Failed to create reservation:", error);
+      alert("Failed to create reservation. Please try again later.");
+    }
+  };
+
   const router = useRouter();
   const restUUID = router.query.restUUID as string;
+  
+  const [openDialog, setOpenDialog] = useState(false);
+  const [confirmation_code, setConfirmationCode] = useState("");
 
   const [restaurantInfo, setRestaurantInfo] = useState({
     name: "",
@@ -94,7 +109,11 @@ function ConfirmReservation() {
         const body = response.body;
         const data = body ? JSON.parse(body) : {};
 
-        if (data.restaurant && Array.isArray(data.restaurant) && data.restaurant.length > 0) {
+        if (
+          data.restaurant &&
+          Array.isArray(data.restaurant) &&
+          data.restaurant.length > 0
+        ) {
           const restaurant = data.restaurant[0];
           setRestaurantInfo({
             name: restaurant.restName || "",
@@ -129,6 +148,19 @@ function ConfirmReservation() {
         </Link>
       </div>
 
+      <CustomDialog isOpen={openDialog} onClose={() => {setOpenDialog(false); router.push("/");}}>
+        <div className="centering-div">
+          <h1>Reservation created successfully!</h1>
+          <h2>Confirmation code: <strong>{confirmation_code}</strong></h2>
+          <button
+            className="btn_secondary"
+            onClick={() => {setOpenDialog(false); router.push("/");}}
+          >
+            Okay
+          </button>
+        </div>
+      </CustomDialog>
+
       <div className="centering-div login-fields">
         <h1>{restaurantInfo.name}</h1>
         <p>{restaurantInfo.address}</p>
@@ -160,7 +192,12 @@ function ConfirmReservation() {
                   alert("Invalid start time");
                   return;
                 }
-                const data = await findOpenTable(restUUID, router.query.numberOfPeople as string, startTime, startTime + 1); // modify to get benchUUID of a bench that can be reserved. 
+                const data = await findOpenTable(
+                  restUUID,
+                  router.query.numberOfPeople as string,
+                  startTime,
+                  startTime + 1
+                ); // modify to get benchUUID of a bench that can be reserved.
                 if (!data || !data.body) {
                   alert("No table available");
                   router.push("/");
@@ -192,7 +229,9 @@ function ConfirmReservation() {
 
                 const e_mail = email; // Replace with actual email input
                 const confirmationCode = generateConfirmationCode(); // Example, generate dynamically if needed
-                const groupSize = parseInt(router.query.numberOfPeople as string); // Convert to number
+                const groupSize = parseInt(
+                  router.query.numberOfPeople as string
+                ); // Convert to number
 
                 createReservation(
                   reservationUUID,
@@ -212,7 +251,7 @@ function ConfirmReservation() {
             Reserve
           </button>
           <span style={{ margin: "0 10px" }}></span>
-          <button className="btn_primary" onClick={() => { }}>
+          <button className="btn_primary" onClick={() => {}}>
             Cancel
           </button>
         </div>

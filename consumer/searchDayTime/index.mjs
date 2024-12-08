4e88dadc32674bfd92859979f15efc1fb459a714
CarlosGir <cegiraltortiz@wpi.edu>
2024-12-08 15:13:48 -0500

    searchDayTime 1.1 Not Tested Yet

--- consumer/searchDayTime/index.mjs
+++ consumer/searchDayTime/index.mjs
@@ -11,8 +11,8 @@ export const handler = async (event) => {
     });
 
     const numberToList = parseInt(event.numberToList) || 5; // Default to 5 records
-    const numberPeople = parseInt(event.groupSize);
-    const dateTime = event.reservation_date_time;
+    const dateTime = event.dateTime;
+    const numSeats = parseInt(event.numSeats);
     
     const dateTimeModClosed = dateTime.slice(0,10); //Test this to make sure it works, might have to make it into the year-month-day object
     const dateTimeModRes = dayjs(dataTime).set("hour", 12).set("minute", 0);
@@ -20,7 +20,7 @@ export const handler = async (event) => {
 
     const query1="SELECT * FROM Restaurants AND isActive = 1 AND openingHour <= ? AND closingHour >= ?"; 
     const query2="SELECT * FROM ClosedDays WHERE closedDate = ?";
-    const query3="SELECT * FROM Reservations WHERE reservationDateTime = ? AND restUUID=?";
+    const query3="SELECT * FROM Reservations WHERE reservationDateTime = ?";
     const query4="SELECT * FROM Benches WHERE restUUID=?"
 
 
@@ -40,45 +40,58 @@ export const handler = async (event) => {
 
     try {
 
-        let dict = {};
-        let body = [];
-
         const restaurants = await queryPromise(query1,[dateTimeHour]);
         const closedDays = await queryPromise(query2,[dateTimeModClosed]);
         let filterList1 = [];
         let filterList2 = [];
         
-        for(var i = 0; i < restaurants.lenght; i++){
-            for(var j = 0; j < closedDays.lenght; j++){
-                if(Restaurant[i].restUUID != ClosedDay[j].restUUID){
-                    filterList1.push(restaurant[i])
+        for(var i = 0; i < restaurants.length; i++){
+            for(var j = 0; j < closedDays.length; j++){
+                if(restaurants[i].restUUID != closedDays[j].restUUID){
+                    filterList1.push(restaurants[i])
                 }
             }
         }
 
-
-        const closedByReservation = await queryPromise(query3, [dateTimeModRes, filterList1.restUUID]); // Need to make an arry and then map out the atribute
+        const closedByReservation = await queryPromise(query3, [dateTimeModRes]); 
         const checkingTables = await queryPromise(query4, [filterList1.restUUID])
 
-        for(var i = 0; i < filterList1.lenght; i++){
-            for(var j = 0; j < closedByReservation.lenght; j++){
-                if(Restruant[i].restUUID != Reservation.restUUID[j]){
-                    filterList2.push(restaurant[i]);
+        for(var i = 0; i < filterList1.length; i++){
+            for(var j = 0; j < closedByReservation.length; j++){
+                if(filterList1[i].restUUID != closedByReservation[j].restUUID){
+                    filterList2.push(filterList1[i]);
                     break;
                 }
-                for(var k = 0; k < checkingTables.lenght; k++){
-                    if(Restaurant[i].restUUID == Benches[k].restUUID){
-                        if(Benches[k].benchUUID != Reservation.benchUUID[j]){
-                            filterList2.push(restaurant[i]);
+                for(var k = 0; k < checkingTables.length; k++){
+                    if(filterList1[i].restUUID == checkingTables[k].restUUID){
+                        if(checkingTables[k].benchUUID != closedByReservation[j].benchUUID && checkingTables[k].numSeats >= numSeats){
+                            filterList2.push(filterList1[i]);
                             break;
                         }
                     }
                 }
             }
         }
-
-    } catch (error) {
         
+
+
+        response = {
+            statusCode: 200,
+            body:JSON.stringify({dateTime: dateTime, numSeats: numSeats, Restaurants: filterList2.slice(0,numberToList)})
+            
+        };
+
+        
+    } catch (error) {
+        console.error("ERROR:", error);
+        response = {
+            statusCode: 400,
+            body: JSON.stringify({ error: error.message })
+        };
+        
+    } finally {
+        // Close the pool after all operations are done
+        pool.end();
     }
 
 
